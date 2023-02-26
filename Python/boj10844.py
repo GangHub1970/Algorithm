c78848f87e85c099476842b41ae84068544fe571
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-02-27 08:32:06 +0900

    BOJ_10844(다이나믹 프로그래밍)

---
+++ Python/boj10844.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+dp = [[0 for _ in range(10)] for _ in range(100)]
+
+dp[0] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+for i in range(1, 100):
+  for j in range(10):
+    if j == 0:
+      dp[i][j] = dp[i - 1][j + 1]
+      continue
+    if j == 9:
+      dp[i][j] = dp[i - 1][j - 1]
+      continue
+    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+
+print(sum(dp[n - 1]) % 1_000_000_000)
