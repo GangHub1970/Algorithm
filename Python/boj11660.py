03acf365c2de37ae12c610e6aac192b34e8b49a9
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-03-13 21:20:38 +0900

    BOJ_11660(다이나믹 프로그래밍)

---
+++ Python/boj11660.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+board = []
+for _ in range(n):
+  board.append(list(map(int, input().split())))
+
+dp = [[0] * (n + 1) for _ in range(n + 1)]
+dp[1][1] = board[0][0]
+for i in range(2, n + 1):
+  dp[1][i] = dp[1][i - 1] + board[0][i - 1]
+  dp[i][1] = dp[i - 1][1] + board[i - 1][0]
+
+for i in range(2, n + 1):
+  for j in range(2, n + 1):
+    dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + board[i - 1][j - 1] - dp[i - 1][j - 1]
+
+for _ in range(m):
+  r1, c1, r2, c2 = map(int, input().split())
+  
+  print(dp[r2][c2] - dp[r2][c1 - 1] - dp[r1 - 1][c2] + dp[r1 - 1][c1 - 1])
+  
